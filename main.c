8893d4419c57018a8e498fae275b10eac586e5ce
Ganganthi <elithilu@hotmail.com>
2019-11-05 21:24:03 -0300

    Creating repository

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "Nokia5110.h"
+
+
+/*
+ *  Perguntas:
+ *
+ *  a)  1- ~SYNC (PB4)
+ *      2- SCLK  (PA2)
+ *      3- Din   (PA7)
+ *
+ *  b) Maximo de 50 MHz
+ *
+ *  c)
+ *
+ *
+ */
+
+int main(void)
+{
+
+	return 0;
+}
