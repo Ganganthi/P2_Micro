617c14da31d2e93c154044fa160541dbb605615f
Ganganthi <53975756+Ganganthi@users.noreply.github.com>
2019-11-05 22:33:30 -0300

    Added question
    Added questions from topics I did not understand.

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "Nokia5110.h"
 
+/*
+Perguntas para o monitor:
+
+1- Usar o UART significa que tem q digitar no terminal? Melhor usar UART ou botoes, que sao mais faceis?
+
+2- Nao entendi o que sao os parametros de amplitude e frequencia, nao entendi a mudanca no display.
+
+3- As ondas geradas tem que se mover ou podem ser estáticas? Se movem, a frequência que interfere na velocidade de movimento?
+
+4- As mudanças periódicas são geradas por interrupt de timers?
+
+*/
 
 /*
  *  Perguntas:
@@ -17,6 +29,22 @@
 
 int main(void)
 {
+	// Habilita clock de 80 MHz
+	SysCtlClockSet(SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_SYSDIV_2_5);
 
+	//Habilita periféricos
+
+
+	// Habilita interrupts
+
+
+	// Configura timers
+
+
+	//Inicializa display
+	Nokia5110_Init();
+    Nokia5110_Clear();
+    
+	while(1);
 	return 0;
 }
